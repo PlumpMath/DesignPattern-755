7800a8bfc4a12aa63475b92eae76bfe28394fe2b
Jeremy Song <jeremy.song@me.com>
2014-05-19 14:24:28 -0400

    Add chain of responsibility pattern

---
+++ src/chainOfResponsibility/ConsoleLogger.java
@@ -0,0 +1,16 @@
+package chainOfResponsibility;
+
+/**
+ * Output log message to console
+ */
+public class ConsoleLogger extends AbstractLogger{
+
+    public ConsoleLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+}
